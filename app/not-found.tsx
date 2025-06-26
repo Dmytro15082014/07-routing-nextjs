5b7729bda8baab6eceb518b6d2c819df1fe26f59
Dmytro Teren <leo20091986@gmail.com>
2025-06-26 21:53:21 +0300

    set not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,17 @@
 import Link from 'next/link';
-import css from './page.module.css';
+import css from '../components/Home/Home.module.css';
 
 const PageNotFound = () => {
   return (
-    <>
+    <div className={css.notfound}>
       <h1 className={css.title}>404 - Page not found</h1>
       <p className={css.description}>
         Sorry, the page you are looking for does not exist.
       </p>
-      <Link href={'/'}>Go back</Link>
-    </>
+      <Link href={'/'} className={css.tag}>
+        Go back
+      </Link>
+    </div>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import css from './page.module.css';
+import css from '../components/Home/Home.module.css';
 
 const Home = () => {
   return (
